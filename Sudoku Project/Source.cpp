0e774228674f5fc37fb83288a0acd240beb69c50
AidenFen <aidenfen@gmail.com>
2021-11-27 06:20:39 -0600

    added 3D array

--- Sudoku Project/Source.cpp
+++ Sudoku Project/Source.cpp
@@ -1,35 +1,87 @@
 #include <iostream>
 #include <iomanip>
-//#include <string>
-// #include <>?
+#include <string>
+
+
+// I want to take a 9x9 sudoku puzzle, store it as a 9x9 array, and generate the 1st round of possible candidates for the missing numbers.
+/*
+steps:
+1. create a 9x9 array that is defined as it is created.
+2. attach another dimension to the 9x9. This is the candidate dimension. Array is now [9x9x9] or [Row x Column x Candidate]
+3. create a function that uses the [Row x Column] dimensions and determines the values for the [Candidate] dimension.
+4. print the [Row x Column] dimensions and the [Candidate] dimension
+
+Plans for steps:
+3. finding the [Candidate]'s values.
+    a. for i in Row[i] Column[i] as box_value:
+        if box_value.isDigit():
+            set Candidate to nothing or box_value // might be useful for it to set to ONLY 1 value (box_value)
+
+        if box_value == "-": find the value's that the box CAN'T be equal to. and numbers in the same box, row or column.
+
+*/
+
 
 using namespace std;
 
 // 1st array is a 9x9
 // 2nd array to hold possible values
 
-const int min = 1;
-const int max = 9;
 
+// square grid settings:
+const int SIZE = 9;
 
-void printArray(int arr[9][9]);
-void findCandidates(int arr[9][9]);
+const int MIN_VALUE = 1;
+const int MAX_VALUE = 9;
+
+//int arr1[1] = {1};
+//int arr2[2][1] =   { {1}, {2} };
+//int arr3[3][2][1] = { { {1}, {2} }, { {3}, {4} }, { {5}, {6} } };
+
+void printArray(char arr[SIZE][SIZE+1][SIZE]);
+void findCandidates(char arr[SIZE][SIZE][SIZE]);
 
 int main() {
-    int sudokuArray[9][9] = { 
+    // [row][col] --> [row][col][candidates]. 
+    // defines [0][size][size]
+    // i want the starting numbers to be the first array, 
 
-        {0,0,0, 0,7,0, 0,2,0},
-        {0,8,0, 0,0,0, 0,3,0},
-        {6,0,0, 0,0,4, 7,0,8},
+    // row value, column value, 
 
-        {0,0,0, 0,0,0, 0,0,0},
-        {0,6,0, 0,3,0, 0,0,1},
-        {4,0,0, 5,0,0, 0,0,0},
 
-        {0,0,9, 0,0,3, 0,0,0},
-        {0,1,0, 0,9,0, 6,0,7},
-        {0,0,0, 0,0,0, 2,0,0}
-    };
+                                //};
+    char sudokuArray[SIZE][SIZE+1][SIZE] = {
+
+        { {'-', '-', '-',  '-', '7', '-',  '-', '2', '-'} },
+        { {'-', '8', '-',  '-', '-', '-',  '-', '3', '-'} },
+        { {'6', '-', '-',  '-', '-', '4',  '7', '-', '8'} },
+
+        { {'-', '-', '-',  '-', '-', '-',  '-', '-', '-'} },
+        { {'-', '6', '-',  '-', '3', '-',  '-', '-', '1'} },
+        { {'4', '-', '-',  '5', '-', '-',  '-', '-', '-'} },
+
+        { {'-', '-', '9',  '-', '-', '3',  '-', '-', '-'} },
+        { {'-', '1', '-',  '-', '9', '-',  '6', '-', '7'} },
+        { {'-', '-', '-',  '-', '-', '-',  '2', '-', '-'} }
+        
+    }; // third dimension isn't defined. it needs to be set to 1 through 9?
+
+
+    int count = 0;
+    for (int row = 0; row < SIZE; row++) {
+        for (int column = 1; column < SIZE+1; column++) {
+            for (int candidate = 0; candidate < SIZE; candidate++) {
+                // assign numbers 1 through 9
+                sudokuArray[row][column][candidate] = candidate + 49; // starts with 1 when i=0
+                cout << sudokuArray[row][column][candidate] << " ";
+                count++;
+            }
+            //cout << endl;
+        }
+        cout << endl;
+    }
+    cout << "count: " << count << endl;
+    cout << "printing sudoku array: " << endl;
 
     // print initial arary.
     printArray(sudokuArray);
@@ -45,24 +97,25 @@ RULES:
 
 */
 
-void findCandidates(int arr[9][9]) {
+void findCandidates(char arr[SIZE][SIZE][SIZE]) {
 
 }
 
-void printArray(int sudokuArray[9][9]) {
-    string top_bar = "  -----   -----   -----  ";
+void printArray(char sudokuArray[SIZE][SIZE+1][SIZE]) {
+    //char top_bar = "  -----   -----   -----  ";
+    //top_bar = "";
 
-    // print 
-    cout << top_bar << endl;
-    for (int x = 0; x < 9; x++) {
-        if (x != 0 && x % 3 == 0) { cout << top_bar << endl; }
-        for (int y = 0; y < 9; y++) {
-            if (y % 3 == 0) { cout << "| "; }
-            cout << sudokuArray[x][y] << " ";
+    cout << endl;
+    for (int row = 0; row < SIZE; row++) {
+        if (row != 0 && row % 3 == 0) { cout << endl; }
+        for (int column = 0; column < 9; column++) {
+            if (column % 3 == 0) { cout << "  "; }
+            cout << sudokuArray[row][0][column] << " ";
         }
-        cout << "|" << endl;
+        cout << endl;
     }
-    cout << top_bar << endl;
+    cout << "here is the junk values for [1][1][1]: " << sudokuArray[0][0][0];
+    //cout << top_bar << endl;
 }
 
 /*
